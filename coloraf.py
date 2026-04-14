2a07a987a28ac60dac716a1dbcadeff00fd5c4de
yf793 <evancaxh@gmail.com>
2026-04-14 12:00:20 -0400

    Update coloraf.py

--- coloraf.py
+++ coloraf.py
@@ -19,10 +19,17 @@ def coloraf(selection="all"):
     The name of the selection/object to color by pLDDT. Default: all
     """
 
-    cmd.color("blue", f"({selection}) and b > 90")
-    cmd.color("cyan", f"({selection}) and b < 90 and b > 70")
-    cmd.color("yellow", f"({selection}) and b < 70 and b > 50")
-    cmd.color("orange", f"({selection}) and b < 50")
+    # Define custom colors from hex, closer to AF coloring system
+    cmd.set_color("af_blue", hex_to_rgb("#106DFF"))
+    cmd.set_color("af_cyan", hex_to_rgb("#10CFF1"))
+    cmd.set_color("af_yellow", hex_to_rgb("#F6ED12"))
+    cmd.set_color("af_orange", hex_to_rgb("#EF821E"))
+
+    # Modified cutoff values so they won't be ignored
+    cmd.color("af_blue",   f"({selection}) and b > 89.999")
+    cmd.color("af_cyan",   f"({selection}) and b < 90 and b > 69.999")
+    cmd.color("af_yellow", f"({selection}) and b < 70 and b > 49.999")
+    cmd.color("af_orange", f"({selection}) and b < 50")
 
 
 cmd.extend("coloraf", coloraf)
